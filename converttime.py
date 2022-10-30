33e3cf45542e7a7bbec30bcdf55ed13012928e3e
Roberto Beán <robcbean@gmail.com>
2022-10-30 10:56:47 +0100

    testing

---
+++ converttime.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from datetime import timedelta
+from pytz import timezone
+
+
+def get_diff_from_utc(date_to_convert: datetime, tzname: str = "Europe/Madrid") -> timedelta:
+    tz: str = timezone(tzname)
+    utc: str = timezone('UTC')
+    utc.localize(datetime.now())
+    delta: timedelta = utc.localize(date_to_convert) - tz.localize(date_to_convert)
+    return delta
+
+
+def convert_to_utc(date_to_convert: datetime, tzname: str = "Europe/Madrid") -> datetime:
+    time_delta: timedelta = get_diff_from_utc(date_to_convert=date_to_convert, tzname=tzname)
+    ret: datetime = date_to_convert - time_delta
+    return ret
+
+
+def convert_from_utf(date_to_convert: datetime, tzname: str = "Europe/Madrid") -> datetime:
+    time_delta: timedelta = get_diff_from_utc(date_to_convert=date_to_convert, tzname=tzname)
+    ret: datetime = date_to_convert + time_delta
+    return ret
