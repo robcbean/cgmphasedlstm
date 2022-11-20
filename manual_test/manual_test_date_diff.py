85e4727a672c882ee5ac0451bcc916411fc3310d
Roberto Beán <robcbean@gmail.com>
2022-11-20 16:23:42 +0100

    test get machine tz

--- manual_test/manual_test_date_diff.py
+++ manual_test/manual_test_date_diff.py
@@ -5,5 +5,6 @@ date_time_utc: datetime.datetime = datetime.datetime.now() + datetime.timedelta(
 date_time_local: datetime.datetime = \
     converttime.convert_from_machine_to_tz(date_to_convert=date_time_utc, tzname="Europe/Madrid")
 
+print(f"{converttime.get_machine_tz()}")
 print(f"date_time_utc:{date_time_utc}\tdate_time_local{date_time_local}")
 
